63879a812f33a116029ee276da77c1e5cd8274bf
Ratnakar <ratnakar.rao.mallayagari@oracle.com>
2020-10-12 17:12:11 +0530

    resolve deployment issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,13 +9,6 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     console.log(`Node created of type "${node.internal.type}`)
     if (node.internal.type === `MarkdownRemark`) {
         console.log("**************"+node.parent.fileName+"***************")
-        const slug = createSlug(createFilePath({ node, getNode, basePath: `docs` }))
-
-        createNodeField({
-            node,
-            name: `slug`,
-            value: slug,
-        })
     }
 }
 
