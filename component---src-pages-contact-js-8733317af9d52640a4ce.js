e20652b29b54531057a53c0047bdb4b1c041a988
ratnakar-m <32613045+ratnakar-m@users.noreply.github.com>
2020-10-19 08:32:18 +0000

    Deploying to master from  @ 33f39d899e3a0176e2f5c735241178f04b626bd3 🚀

--- component---src-pages-contact-js-8733317af9d52640a4ce.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{"Cuy+":function(a,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return c}));var l=t("q1tI"),n=t.n(l),r=t("Bl7J");function c(){return n.a.createElement("div",null,n.a.createElement(r.a,{headerText:"Contact"},n.a.createElement("h1",null,"I'd love to talk! Email me at the address below"),n.a.createElement("p",null,n.a.createElement("a",{href:"mailto:ratnakar.mallayagari@gmail.com"},"ratnakar.mallayagari@gmail.com"))))}}}]);
-//# sourceMappingURL=component---src-pages-contact-js-8733317af9d52640a4ce.js.map
